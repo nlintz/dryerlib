bd759a59bf815fc1dd44c52ec288d31b6ccc595c
Nathan Lintz <nlintz@gmail.com>
2016-04-04 22:04:02 -0400

    fixed accel.py

--- accel.py
+++ accel.py
@@ -10,25 +10,6 @@ axis2addr = {
 }
 
 
-def read_byte(adr):
-    return bus.read_byte_data(address, adr)
-
-
-def read_word(adr):
-    high = bus.read_byte_data(address, adr)
-    low = bus.read_byte_data(address, adr+1)
-    val = (high << 8) + low
-    return val
-
-
-def read_word_2c(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
-
-
 def get_y_rotation(x,y,z):
     radians = math.atan2(x, dist(y,z))
     return -math.degrees(radians)
@@ -48,4 +29,22 @@ class Accelerometer(object):
         self.bus.write_byte_data(self.address, power_mgmt_1, 0)
 
     def read_accel(self, axis):
-        return read_word_2c(axis2addr[axis]) / 16384.0
+        return self.read_word_2c(axis2addr[axis]) / 16384.0
+
+    def read_byte(self, adr):
+        return self.bus.read_byte_data(address, adr)
+
+
+    def read_word(self, adr):
+        high = self.bus.read_byte_data(address, adr)
+        low = self.bus.read_byte_data(address, adr+1)
+        val = (high << 8) + low
+        return val
+
+    def read_word_2c(self, adr):
+        val = self.read_word(adr)
+        if (val >= 0x8000):
+            return -((65535 - val) + 1)
+        else:
+            return val
+
