e755197253de29e7725c37f19b7eb57440a8a799
Nathan Lintz <nlintz@gmail.com>
2016-04-06 00:01:39 -0400

    rewrote controller fsm

--- controller.py
+++ controller.py
@@ -1,22 +1,59 @@
 import dryer
 import time
+import dsp
+# import numpy as np
 
-d = dryer.Dryer()
-reading_buffer_size = 5
-off_sleep_duration = 2
-on_sleep_duration = 1
-on_threshold = 0.1
-state = False
 
-for i in range(1000):
-    d.collect_sample()
-    if len(d.previous_readings) > reading_buffer_size:
-	print d.get_lastn_norm_integral(reading_buffer_size)
-        if d.get_lastn_norm_integral(reading_buffer_size) < on_threshold:
-            if state is True:
-                print ("laundry is done")
-        if state == False:
-            time.sleep(off_sleep_duration)
-        else:
-            time.sleep(on_sleep_duration)
-    print d.get_lastn_norm_integral(reading_buffer_size)
+class Controller(object):
+    def __init__(self, off_sleep_duration=2, on_sleep_duration=1,
+                 initial_state="off", on_threshold=0.1, num_samples=100,
+                 mavg_samples=10):
+        self.dryer = dryer.Dryer()
+        self.state_machine = {"on":self._on_state,
+                              "off":self._off_state,
+                              "alert":self._alert_state}
+        self.off_sleep_duration = off_sleep_duration
+        self.on_sleep_duration = on_sleep_duration
+        self.state = initial_state
+        self.on_threshold = on_threshold
+        self.num_samples = num_samples
+        self.mavg_samples = mavg_samples
+
+    def compute_norm_sum(self):
+        samples = self.dryer.get_readings(self.mavg_samples+self.num_samples)
+        norm = dsp.compute_norm(samples)
+        norm_lpf = dsp.moving_average(norm_lpf, n=self.mavg_samples)
+        norm_lpf_slice = norm_lpf[self.mavg_samples:self.mavg_samples+self.num_samples]
+        norm_sum = norm_lpf_slice.sum()
+        # norm_sum = np.random.randn()
+        return norm_sum
+
+
+    def _on_state(self):
+        norm_sum = self.compute_norm_sum()
+        print norm_sum
+        if norm_sum < self.on_threshold:
+            self.state = "alert"
+        time.sleep(self.on_sleep_duration)
+
+    def _alert_state(self):
+        print "tweet!"
+        self.state = "off"
+
+    def _off_state(self):
+        norm_sum = self.compute_norm_sum()
+        print norm_sum
+        if norm_sum > self.on_threshold:
+            self.state = "on"
+
+        time.sleep(self.on_sleep_duration)
+
+    def run(self):
+        while True:
+            print self.state
+            self.state_machine[self.state]()
+
+
+if __name__ == "__main__":
+    c = Controller()
+    c.run()
--- dryer.py
+++ dryer.py
@@ -1,23 +1,20 @@
 from accel import Accelerometer
-from dsp import *
+import dsp
 
 class Dryer(object):
     def __init__(self):
         self.accelerometer = Accelerometer()
         self.accelerometer.initialize()
 
-        self.previous_readings = []
-
     def collect_sample(self):
         x = self.accelerometer.read_accel('x')
         y = self.accelerometer.read_accel('y')
         z = self.accelerometer.read_accel('z')
-	return {'x': x, 'y': y, 'z': z}
-        self.previous_readings.append({'x': x, 'y': y, 'z': z})
 
-    def get_lastn_readings(self, n):
-        return self.previous_readings[-n:]
+        return {"x": x, "y": y, "z": z}
 
-    def get_lastn_norm_integral(self, n):
-        lastn_norms = preprocess_norm(df_to_mat(self.get_lastn_readings(n)))
-        return np.mean(lastn_norms)
+    def get_readings(self, num_samples):
+        readings = []
+        for i in range(num_samples):
+            readings.append(self.collect_sample())
+        return dsp.df_to_mat(readings)
